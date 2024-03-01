9688235df4c9a4300c1dee1e70f55b25e377338d
GopaltiwariIET <gopaltiwari28122003@gmail.com>
2024-03-01 20:28:10 +0530

    fix minor issues

--- public/index.js
+++ public/index.js
@@ -17,61 +17,57 @@ $("#searchCity").keypress(function (event) {
     }
   }
 });
-
-function fetchData(city = "Lucknow") {
-  // const key = "CPFSV8JM96NUGHLF4WPXHZH43";
-  const key = "FP6U7553EBYTTFR8MDSEG4LCB";
-  fetch(
+const key = "CPFSV8JM96NUGHLF4WPXHZH43";
+// const key = "FP6U7553EBYTTFR8MDSEG4LCB";
+async function fetchData(city = "Lucknow", key) {
+  const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&key=${key}&contentType=json`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const {
-        conditions,
-        datetime,
-        feelslike,
-        humidity,
-        pressure,
-        sunrise,
-        sunset,
-        temp,
-        visibility,
-        winddir,
-        windspeed,
-        tempmax,
-        tempmin,
-        description,
-      } = data.days[0];
-      //
-      $("#address").text(data.resolvedAddress);
-      $("#temp").html(temp + "°C");
-      $("#weatherIcon").attr("src", `icons/${conditions}.png`);
-      $("#conditions").text(conditions);
-      $("#dateTime").text(`Time: ${datetime}`);
-      $("#wind").html(
-        "<strong>Wind: </strong>" + windspeed + " km/h - " + winddir + "°"
-      );
-      $("#humidity").html("<strong>Humidity: </strong>" + humidity + "%");
-      $("#pressure").html("<strong>Pressure: </strong>" + pressure + " hPa");
-      $("#visibility").html(`<strong>Visibility: </strong>${visibility} km`);
-      $("#sunrise").html(`<strong>Sunrise: </strong>${sunrise}`);
-      $("#sunset").html(`<strong>Sunset: </strong>${sunset}`);
-      $("#feelslike").html("<strong>Feels Like: </strong>" + feelslike + "°C");
+  );
+  const data = await response.json();
+  console.log(data);
+  const {
+    conditions,
+    datetime,
+    feelslike,
+    humidity,
+    pressure,
+    sunrise,
+    sunset,
+    temp,
+    visibility,
+    winddir,
+    windspeed,
+    tempmax,
+    tempmin,
+    description,
+  } = data.days[0];
 
-      $("#sunrise2").html(`<strong>Sunrise: </strong>${sunrise}`);
-      $("#sunset2").html(`<strong>Sunset: </strong>${sunset}`);
-      $("#max-temp").html("<strong>Max Temp: </strong>" + tempmax + "°C");
-      $("#min-temp").html("<strong>Min Temp: </strong>" + tempmin + "°C");
-      $("#desc").text(description);
+  $("#address").text(data.resolvedAddress);
+  $("#temp").html(temp + "°C");
+  $("#weatherIcon").attr("src", `icons/${conditions}.png`);
+  $("#conditions").text(conditions);
+  $("#dateTime").text(`Time: ${datetime}`);
+  $("#wind").html(
+    "<strong>Wind: </strong>" + windspeed + " km/h - " + winddir + "°"
+  );
+  $("#humidity").html("<strong>Humidity: </strong>" + humidity + "%");
+  $("#pressure").html("<strong>Pressure: </strong>" + pressure + " hPa");
+  $("#visibility").html(`<strong>Visibility: </strong>${visibility} km`);
+  $("#sunrise").html(`<strong>Sunrise: </strong>${sunrise}`);
+  $("#sunset").html(`<strong>Sunset: </strong>${sunset}`);
+  $("#feelslike").html("<strong>Feels Like: </strong>" + feelslike + "°C");
 
-      $("#tableBody").html(
-        data.days[0].hours.map((hour) => {
-          return `<tr><td>${hour.datetime}</td><td>${hour.temp}°C</td><td>${hour.humidity}%</td><td>${hour.pressure} hPa</td><td>${hour.visibility} km</td><td>${hour.conditions}</td><td>${hour.windspeed} km/h - ${hour.winddir}°</td></tr><tr>`;
-        })
-      );
+  $("#sunrise2").html(`<strong>Sunrise: </strong>${sunrise}`);
+  $("#sunset2").html(`<strong>Sunset: </strong>${sunset}`);
+  $("#max-temp").html("<strong>Max Temp: </strong>" + tempmax + "°C");
+  $("#min-temp").html("<strong>Min Temp: </strong>" + tempmin + "°C");
+  $("#desc").text(description);
+
+  $("#tableBody").html(
+    data.days[0].hours.map((hour) => {
+      return `<tr><td>${hour.datetime}</td><td>${hour.temp}°C</td><td>${hour.humidity}%</td><td>${hour.pressure} hPa</td><td>${hour.visibility} km</td><td>${hour.conditions}</td><td>${hour.windspeed} km/h - ${hour.winddir}°</td></tr><tr>`;
     })
-    .catch((error) => console.error("Error:", error));
+  );
 }
 
 fetchData();
